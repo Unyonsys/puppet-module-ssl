6671df801b10984d009ea82be64c5c539e3152d2
Bruno Léon <bruno.leon@unyonsys.com>
2011-11-11 13:48:21 -0500

    Added ssl-cert group. Notify apache2 when ssl config changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,13 @@ class ssl::common {
   include ssl::variables
   package { 'openssl': ensure => present }
 
+  group { 'ssl-cert':
+    ensure => present,
+    gid    => 361,
+    system => true,
+  }
+
+
   file { "${ssl::variables::ssl_root}/services" :
     ensure  => directory,
     mode    => 0644,
